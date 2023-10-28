6ce864bc33e77723cb250b8a938d563bfcd7cf28
IZoom7 <crazygame1908@gmail.com>
2023-10-28 18:03:33 +0700

    twelveth commit

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -70,7 +70,7 @@ if ($result->num_rows > 0) {
                     <td>".$row["pet_breed"]."</td>
                     <td>".$row["age"]."</td>
                     <td>".$row["pet_detail"]."</td>
-                    <td><img src='data:imgage/jpg;base64," . base64_encode($row["pet_image"]) . "'></td>
+                    <td><img src='data:imgage/*;base64," . base64_encode($row["pet_image"]) . "'></td>
                     <td>".$row["owner_id"]."</td>
               </tr>";
     }
