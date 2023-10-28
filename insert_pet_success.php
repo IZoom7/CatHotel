0ac0adf1344d1deadfef484232939fbc38b45019
IZoom7 <crazygame1908@gmail.com>
2023-10-28 23:12:20 +0700

    twelveth commit

--- insert_pet_success.php
+++ insert_pet_success.php
@@ -1,42 +1,47 @@
 <body style="font-family: 'Itim', cursive;">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script async src="//pic.in.th/sdk/pup.js" data-url="https://pic.in.th/upload"></script>
 
     <?php
 session_start();
 require "dbconn.php";
 
+// Check if the pet_image URL is available
+if (isset($_POST['pet_image_url'])) {
+    $pet_image_url = $_POST['pet_image_url'];
+} else {
+    $pet_image_url = ''; // Set to an empty string if no URL is provided
+}
 
-    // File upload path
-$targetDir = "uploaded/";
+// Rest of your form data
+$pet_name = $_POST['pet_name'];
+$age = $_POST['age'];
+$pet_type = $_POST['pet_type'];
+$pet_breed = $_POST['pet_breed'];
+$pet_detail = $_POST['pet_detail'];
+$owner_id = $_POST['owner_id'];
 
-if (isset($_POST['submit'])) {
-    if (!empty($_FILES["pet_image"]["name"])) {
-        $fileName = basename($_FILES["pet_image"]["name"]);
-        $targetFilePath = $targetDir . $fileName;
-        $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+// Your SQL INSERT query
+$sql_insert = "INSERT INTO pets (pet_name, age, pet_type, pet_breed, pet_detail, pet_image, owner_id) 
+              VALUES ('$pet_name', '$age', '$pet_type', '$pet_breed', '$pet_detail', '$pet_image_url', '$owner_id')";
 
+$result = $conn->query($sql_insert);
 
-    $sql_update = "INSERT INTO pets(pet_name,pet_type,pet_breed,age,pet_detail,pet_image,owner_id) VALUES ('$_POST[pet_name]', '$_POST[pet_type]', '$_POST[pet_breed]', '$_POST[age]','$_POST[pet_detail]','$_POST[pet_image]','$_POST[owner_id]')";
+if (!$result) {
+    die("Error: " . $conn->error);
+} else {
+    // Display a success message using SweetAlert
+    echo '<script>
+        Swal.fire({
+            icon: "success",
+            title: "เพิ่มชื่อสัตว์เลี้ยงสำเร็จ",
+            text: "รอซักครู่นะเหมียว..."
+        }).then(function() {
+            window.location.href = "https://petvilla.online/home_session.php";
+        });
+    </script>';
+}
+?>
 
-
-    $result = $conn->query($sql_update);
-
-    if (!$result) {
-        die("Error God Damn it : " . $conn->error);
-    } else {
-
-        echo '<body><script>
-Swal.fire({
-    icon: "success",
-    title: "เพิ่มชื่อสัตว์เลี้ยงสำเร็จ",
-    text: "รอซักครู่นะเหมียว..."
-});
-</script></body>';
-        header("refresh: 1; url=https://petvilla.online/home_session.php");
-    }
-
-
-
-    ?>
 
 </body>
