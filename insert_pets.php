4b082eddbaacc2db20437cc36f05f1326f39f740
IZoom7 <crazygame1908@gmail.com>
2023-10-29 01:53:12 +0700

    twelveth commit

--- insert_pets.php
+++ insert_pets.php
@@ -145,8 +145,8 @@ if (isset($_SESSION['username'])) {
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">รายละเอียดเพิ่มเติม</label>
                 <input type="text" placeholder="เช่น.....โรคประจำตัว อาหารโปรด ยาหรืออาหารที่แพ้" class="form-control"
-                    id="exampleFormControlTextarea1"  name="pet_detail" style="width: 130%; /* กว้างเต็มหน้าจอ */
-  height: 30px; /* ความสูงเล็ก (ปรับค่าตามต้องการ) */
+                    id="exampleFormControlTextarea1"  name="pet_detail" style="width: 100%; /* กว้างเต็มหน้าจอ */
+  height: 300px; /* ความสูงเล็ก (ปรับค่าตามต้องการ) */
   padding: 5px; /* ขอบภายใน */
   border: 1px solid #ccc; /* เส้นขอบ */
   border-radius: 5px; /* มุมขอบ */
