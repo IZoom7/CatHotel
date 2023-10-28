07caef06960e1cd16da94c66c58025757209ba89
IZoom7 <crazygame1908@gmail.com>
2023-10-29 02:22:08 +0700

    sixteenth commit

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -25,6 +25,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="icon" href="img/cat_icon01.png" >
 
+<style>
+    td img
+    {
+        width: 300px;
+        height: 300px;
+    }
+</style>
+
 </head>
 <body>
 
