700948b38795adb14d06bf0df487dfeae24e2414
IZoom7 <crazygame1908@gmail.com>
2023-10-28 21:58:06 +0700

    twelveth commit

--- insert_pet_success.php
+++ insert_pet_success.php
@@ -5,52 +5,61 @@
 session_start();
 require "dbconn.php";
 
-// Check if a file was uploaded
-if ($_FILES['pet_image']['name'] != '') {
-    // Set the upload directory
-    $upload_dir = "uploaded/";
+<?php
 
-    // Get the file extension
-    $file_extension = pathinfo($_FILES['pet_image']['name'], PATHINFO_EXTENSION);
 
-    // Generate a unique filename using the current date and time
-    $new_filename = date("YmdHis") . "." . $file_extension;
+    session_start();
+    require "dbconn.php";
 
-    // Set the path for the uploaded file
-    $upload_path = $upload_dir . $new_filename;
 
-    // Move the uploaded file to the specified directory
-    if (move_uploaded_file($_FILES['pet_image']['tmp_name'], $upload_path)) {
-        // File uploaded successfully
-        $pet_image = $new_filename;
-    } else {
-        // Handle the upload error
-        die("Error uploading file.");
+    $fileupload = $_POST['pet_image']; // รับค่าไฟล์จากฟอร์ม
+    $fileupload = (isset($_POST['pet_image']) ? $_POST['pet_image'] : '');
+
+    //ฟังก์ชั่นวันที่
+    date_default_timezone_set('Asia/Bangkok');
+    $date = date("Ymd");
+    //สุ่มตัวเลข
+    $numrand = (mt_rand());
+    //เพิ่มไฟล์
+    $upload = $_FILES['pet_image'];
+    if ($upload != '') { //not select file
+        //โฟลเดอร์ที่จะupload file เข้าไป
+        $path = "uploaded/";
+        //เอาชื่อไฟล์ออกให้เหลือแต่นามสกุล
+        $type = strrchr($_FILES['uploaded']['name'], ".");
+
+        //ตั้งชื่อไฟล์ใหม่โดยเอาเวลาไว้หน้าชื่อไฟล์เดิม
+        $newname = $date . $numrand . $type;
+        $path_copy = $path . $newname;
+        $path_link = "fileupload/" . $newname;
+
+        //คัดลอกไฟล์ไปเก็บที่เว็บเซิร์ฟเวอร์
+        move_uploaded_file($_FILES['pet_image']['tmp_name'], $path_copy);
     }
-} else {
-    // No file was uploaded, set $pet_image to the existing value (if any)
-    $pet_image = isset($_POST['pet_image']) ? $_POST['pet_image'] : '';
-}
 
-// Rest of your code
-// ...
 
-$sql_update = "INSERT INTO pets (pet_name, pet_type, pet_breed, age, pet_detail, pet_image, owner_id) VALUES ('$_POST[pet_name]', '$_POST[pet_type]', '$_POST[pet_breed]', '$_POST[age]', '$_POST[pet_detail]', '$pet_image', '$_POST[owner_id]')";
 
-$result = $conn->query($sql_update);
+    $sql_update = "INSERT INTO pets(pet_name,pet_type,pet_breed,age,pet_detail,pet_image,owner_id) VALUES ('$_POST[pet_name]', '$_POST[pet_type]', '$_POST[pet_breed]', '$_POST[age]','$_POST[pet_detail]','$newname','$_POST[owner_id]')";
 
-if (!$result) {
-    die("Error: " . $conn->error);
-} else {
-    echo '<body><script>
+
+    $result = $conn->query($sql_update);
+
+    if (!$result) {
+        die("Error God Damn it : " . $conn->error);
+    } else {
+
+        echo '<body><script>
 Swal.fire({
     icon: "success",
     title: "เพิ่มชื่อสัตว์เลี้ยงสำเร็จ",
     text: "รอซักครู่นะเหมียว..."
 });
 </script></body>';
-    header("refresh: 1; url=https://petvilla.online/home_session.php");
-}
-?>
+        header("refresh: 1; url=https://petvilla.online/home_session.php");
+    }
+
+
+
+    ?>
 
 </body>
