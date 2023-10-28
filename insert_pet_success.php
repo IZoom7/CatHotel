d901ba337586a8b827099f74a4ea146cbfe3ef76
IZoom7 <crazygame1908@gmail.com>
2023-10-29 02:13:35 +0700

    fourteenth commit

--- insert_pet_success.php
+++ insert_pet_success.php
@@ -3,28 +3,20 @@
 
     <?php
     require 'dbconn.php';
-    if (isset($_SESSION['username'])) {
-        $username = $_SESSION['username'];
-        
-        $sql_put_owner = "SELECT owner_id FROM owner WHERE owner_username = '$username'";
-        $sql_update = "INSERT INTO pets(pet_name,pet_type,pet_breed,age,pet_detail,pet_image,owner_id) VALUES ('$_POST[pet_name]', '$_POST[pet_type]', '$_POST[pet_breed]', '$_POST[age]', '$_POST[pet_detail]', '$_POST[pet_image]','$sql_put_owner')";
-
-
-        $result = $conn->query($sql_update);
-
-        if (!$result) {
-            die("Error God Damn it : " . $conn->error);
-        } else {
-
-            echo '<body><script>
+    $sql_update = "INSERT INTO pets(pet_name,pet_type,pet_breed,age,pet_detail,pet_image,owner_id) VALUES ('$_POST[pet_name]', '$_POST[pet_type]', '$_POST[pet_breed]', '$_POST[age]','$_POST[pet_detail]','$_POST[pet_image]','$_POST[owner_id]')";
+    $result = $conn->query($sql_update);
+    if (!$result) {
+        die("Error God Damn it : " . $conn->error);
+    } else {
+        echo '<body><script>
 Swal.fire({
     icon: "success",
-    title: "เพื่มรายชื่อสัตว์เลี้ยงสำเร็จ",
+    title: "เพิ่มชื่อสัตว์เลี้ยงสำเร็จ",
     text: "รอซักครู่นะเหมียว..."
 });
 </script></body>';
-            header("refresh: 1; url=http://localhost/CatHotel/home_session.php");
-        }
+        header("refresh: 1; url=https://petvilla.online/home_session.php");
     }
+
     ?>
 </body>
