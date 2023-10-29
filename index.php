8c145e294bff281ee0eeb06f5cf6716b9c3e6560
IZoom7 <crazygame1908@gmail.com>
2023-10-29 22:00:36 +0700

    51st commit

--- index.php
+++ index.php
@@ -23,11 +23,12 @@
         <a href="login.php">แอบส่องน้องๆ</a>
         <a href="login.php">ติดต่อพี่เลี้ยง</a>
         <a href="#">เกี่ยวกับเรา</a>
-        
+
         <a class="form-group">
         <input type="text" class="form-control" placeholder="Search">
         </a>
-    <button type="submit" class="btn btn-default">ค้นหา</button>
+        <button type="submit" class="btn btn-light">ค้นหา</button>
+        
                          
         <a id="loginbtn" href="login.php">เข้าสู่ระบบ</a>
         <a href="register.php">สมัครสมาชิก</a>
