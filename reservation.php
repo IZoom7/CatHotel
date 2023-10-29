aa1c2d04c4956314e3db93b414e782e7b4ba092b
IZoom7 <crazygame1908@gmail.com>
2023-10-29 19:33:04 +0700

    32nd commit

--- reservation.php
+++ reservation.php
@@ -125,7 +125,7 @@ if (isset($_SESSION['username'])) {
     <a href="insert_pets.php">
         <div style="text-decoration: none; color: #707070; width: 250px; height: 295px; display: flex; flex-direction: column; align-items: center; justify-content: center;" class="pet-card">
             <img style="width: 50px; height: 50px;" src="img/plus.png" alt="">
-            <h6 style="margin-top: 10px;">เพิ่มรายชื่อสัตว์เลี้ยง</h6>
+            <h6 style="text-decoration: none; margin-top: 10px;">เพิ่มรายชื่อสัตว์เลี้ยง</h6>
         </div>
     </a>
 </div>
