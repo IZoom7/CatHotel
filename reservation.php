a62e2c4e5a95e74b6ac56fbeaad71ec3cfb7dad9
IZoom7 <crazygame1908@gmail.com>
2023-10-29 21:06:41 +0700

    41st commit

--- reservation.php
+++ reservation.php
@@ -4,7 +4,7 @@
 if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
 
-    $sql = "SELECT * FROM pets INNER JOIN owner ON pets.owner_id = owner.owner_id WHERE owner_username = '$username'";
+    $sql = "SELECT * FROM pets WHERE owner_id = (SELECT owner_id from owner where owner_username = '$username') UNION SELECT * FROM owner WHERE owner_username = '$username";
 
     
     $result = $conn->query($sql);
