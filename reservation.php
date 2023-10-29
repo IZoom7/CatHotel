ee0755511f4fd69abc1c27f9a21c1e50b072ff75
IZoom7 <crazygame1908@gmail.com>
2023-10-29 20:17:00 +0700

    36th commit

--- reservation.php
+++ reservation.php
@@ -124,7 +124,7 @@ if (isset($_SESSION['username'])) {
     // เพิ่ม "เพิ่มรายชื่อสัตว์เลี้ยง" นอกจากลูป
     ?>
     <a style="text-decoration: none;" href="insert_pets.php">
-        <div style="padding: top 12px; text-decoration: none; color: #707070; width: 270px; height: 295px; display: flex; flex-direction: column; align-items: center; justify-content: center;" class="pet-card">
+        <div style="padding-top: 12px; text-decoration: none; color: #707070; width: 270px; height: 295px; display: flex; flex-direction: column; align-items: center; justify-content: center;" class="pet-card">
             <img style="width: 50px; height: 50px;" src="img/plus.png" alt="">
             <h6 style="text-decoration: none; margin-top: 10px;">เพิ่มรายชื่อสัตว์เลี้ยง</h6>
         </div>
