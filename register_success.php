e8627392e8e0c09f917bfa3d22a9dff6f05dcb49
IZoom7 <crazygame1908@gmail.com>
2023-10-30 13:19:05 +0700

    65th commit

--- register_success.php
+++ register_success.php
@@ -5,48 +5,63 @@
 require 'dbconn.php';
 
 if (isset($_POST['owner_name']) && isset($_POST['owner_phone']) && isset($_POST['owner_username']) && isset($_POST['owner_password'])) {
+    $owner_name = $_POST['owner_name'];
+    $owner_phone = $_POST['owner_phone'];
     $owner_username = $_POST['owner_username'];
+    $owner_password = $_POST['owner_password'];
 
-    // ตรวจสอบว่า owner_username มีค่าซ้ำกันหรือไม่
-    $check_username_sql = "SELECT * FROM owner WHERE owner_username = '$owner_username'";
-    $result = $conn->query($check_username_sql);
-
-    if ($result->num_rows > 0) {
-        // ถ้ามี username ซ้ำกันในฐานข้อมูล
+    // ตรวจสอบว่าข้อมูลถูกกรอกครบหรือไม่
+    if (empty($owner_name) || empty($owner_phone) || empty($owner_username) || empty($owner_password)) {
+        // ถ้าข้อมูลไม่ถูกกรอกครบ
         echo '<script>
             Swal.fire({
                 icon: "error",
-                title: "ชื่อผู้ใช้ซ้ำกัน",
-                text: "ชื่อผู้ใช้นี้ถูกใช้ไปแล้ว โปรดเลือกชื่อผู้ใช้อื่น",
+                title: "โปรดใส่ข้อมูลให้ครบ",
+                text: "กรุณากรอกข้อมูลให้ครบทุกช่อง",
             });
             setTimeout(function() {
                 window.location.href = "https://petvilla.online/login.php";
             }, 1000); // นำทางหน้า login.php หลังจาก 1 วินาที
         </script>';
     } else {
-        // ถ้า owner_username ไม่ซ้ำกัน
-        $owner_name = $_POST['owner_name'];
-        $owner_phone = $_POST['owner_phone'];
-        $owner_password = $_POST['owner_password'];
+        // ถ้าข้อมูลถูกกรอกครบ
+        // ตรวจสอบว่า owner_username มีค่าซ้ำกันหรือไม่
+        $check_username_sql = "SELECT * FROM owner WHERE owner_username = '$owner_username'";
+        $result = $conn->query($check_username_sql);
 
-        $sql_insert = "INSERT INTO owner(owner_name, owner_phone, owner_username, owner_password) 
-            VALUES ('$owner_name', '$owner_phone', '$owner_username', '$owner_password')";
-
-        $result = $conn->query($sql_insert);
-
-        if (!$result) {
-            die("Error God Damn it : " . $conn->error);
-        } else {
+        if ($result->num_rows > 0) {
+            // ถ้ามี username ซ้ำกันในฐานข้อมูล
             echo '<script>
                 Swal.fire({
-                    icon: "success",
-                    title: "ลงทะเบียนสำเร็จ",
-                    text: "รอซักครู่นะเหมียว..."
+                    icon: "error",
+                    title: "ชื่อผู้ใช้ซ้ำกัน",
+                    text: "ชื่อผู้ใช้นี้ถูกใช้ไปแล้ว โปรดเลือกชื่อผู้ใช้อื่น",
                 });
                 setTimeout(function() {
                     window.location.href = "https://petvilla.online/login.php";
                 }, 1000); // นำทางหน้า login.php หลังจาก 1 วินาที
             </script>';
+        } else {
+            // ถ้า owner_username ไม่ซ้ำกัน
+            $sql_insert = "INSERT INTO owner(owner_name, owner_phone, owner_username, owner_password) 
+                VALUES ('$owner_name', '$owner_phone', '$owner_username', '$owner_password')";
+
+            $result = $conn->query($sql_insert);
+
+            if (!$result) {
+                die("Error God Damn it : " . $conn->error);
+            } else {
+                echo '<script>
+                    Swal.fire({
+                        icon: "success",
+                        title: "ลงทะเบียนสำเร็จ",
+                        text: "รอซักครู่นะเหมียว..."
+                    });
+                    setTimeout(function() {
+                        window.location.href = "https://petvilla.online/login.php";
+                    }, 1000); // นำทางหน้า login.php หลังจาก 1 วินาที
+                </script>';
+            }
         }
     }
 } else {
@@ -63,5 +78,6 @@ if (isset($_POST['owner_name']) && isset($_POST['owner_phone']) && isset($_POST[
     </script>';
 }
 ?>
+
 
 </body>
