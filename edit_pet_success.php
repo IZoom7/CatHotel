0a3f67cca5c862651b8fcf7d041a64d91689e6f6
IZoom7 <crazygame1908@gmail.com>
2023-10-30 20:12:30 +0700

    93rd commit

--- edit_pet_success.php
+++ edit_pet_success.php
@@ -11,7 +11,7 @@ $sql_update="UPDATE pets SET pet_name='$_POST[pet_name]',
                              pet_breed='$_POST[pet_breed]',
                              age='$_POST[age]',
                              pet_detail='$_POST[pet_detail]',
-                             pet_image='$_POST[pet_image]' WHERE pet_id='$_POST[pet_id]'";
+                             pet_image='$_POST[pet_image]' WHERE pet_id='$_GET[pet_id]'";
 
 $result= $conn->query($sql_update);
 
