7fd23a9b36f0834a05e67ca2230fdbffa3b730b1
IZoom7 <crazygame1908@gmail.com>
2023-10-30 13:09:57 +0700

    64th commit

--- register_success.php
+++ register_success.php
@@ -3,24 +3,59 @@
 
 <?php
 require 'dbconn.php';
-$sql_update = "INSERT INTO owner(owner_name,owner_phone,owner_username,owner_password) VALUES ('$_POST[owner_name]', '$_POST[owner_phone]', '$_POST[owner_username]', '$_POST[owner_password]')";
 
+if (isset($_POST['owner_name']) && isset($_POST['owner_phone']) && isset($_POST['owner_username']) && isset($_POST['owner_password'])) {
+    $owner_username = $_POST['owner_username'];
 
-$result= $conn->query($sql_update);
+    // ตรวจสอบว่า owner_username มีค่าซ้ำกันหรือไม่
+    $check_username_sql = "SELECT * FROM owner WHERE owner_username = '$owner_username'";
+    $result = $conn->query($check_username_sql);
 
-if(!$result) {
-    die("Error God Damn it : ". $conn->error);
+    if ($result->num_rows > 0) {
+        // ถ้ามี username ซ้ำกันในฐานข้อมูล
+        echo '<script>
+            Swal.fire({
+                icon: "error",
+                title: "ชื่อผู้ใช้ซ้ำกัน",
+                text: "ชื่อผู้ใช้นี้ถูกใช้ไปแล้ว โปรดเลือกชื่อผู้ใช้อื่น",
+            });
+        </script>';
+    } else {
+        // ถ้า owner_username ไม่ซ้ำกัน
+        $owner_name = $_POST['owner_name'];
+        $owner_phone = $_POST['owner_phone'];
+        $owner_password = $_POST['owner_password'];
+
+        $sql_insert = "INSERT INTO owner(owner_name, owner_phone, owner_username, owner_password) 
+            VALUES ('$owner_name', '$owner_phone', '$owner_username', '$owner_password')";
+
+        $result = $conn->query($sql_insert);
+
+        if (!$result) {
+            die("Error God Damn it : " . $conn->error);
+        } else {
+            echo '<script>
+                Swal.fire({
+                    icon: "success",
+                    title: "ลงทะเบียนสำเร็จ",
+                    text: "รอซักครู่นะเหมียว..."
+                });
+                setTimeout(function() {
+                    window.location.href = "https://petvilla.online/login.php";
+                }, 1000); // นำทางหน้า login.php หลังจาก 1 วินาที
+            </script>';
+        }
+    }
 } else {
-
-echo '<body><script>
-Swal.fire({
-    icon: "success",
-    title: "ลงทะเบียนสำเร็จ",
-    text: "รอซักครู่นะเหมียว..."
-});
-</script></body>';
-header("refresh: 1; url=https://petvilla.online/login.php");
+    // ถ้าไม่ได้กรอกข้อมูลครบ
+    echo '<script>
+        Swal.fire({
+            icon: "error",
+            title: "โปรดใส่ข้อมูลให้ครบ",
+            text: "กรุณากรอกข้อมูลให้ครบทุกช่อง",
+        });
+    </script>';
 }
-
 ?>
+
 </body>
