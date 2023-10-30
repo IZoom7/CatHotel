c58332ae5fdcbdf60e5bb0101a33e634f49b1bca
IZoom7 <crazygame1908@gmail.com>
2023-10-30 20:17:57 +0700

    93rd commit

--- edit_pet_success.php
+++ edit_pet_success.php
@@ -5,13 +5,12 @@
 
 require 'dbconn.php';
 
-if (isset($_GET['pet_id'])) {
-$sql_update="UPDATE pets SET pet_name='$_POST[pet_name]',
-                             pet_type='$_POST[pet_type]',
-                             pet_breed='$_POST[pet_breed]',
-                             age='$_POST[age]',
-                             pet_detail='$_POST[pet_detail]',
-                             pet_image='$_POST[pet_image]' WHERE pet_id='$_GET[pet_id]'";
+$sql_update="UPDATE pets SET pet_name =$_POST[pet_name]',
+                             pet_type ='$_POST[pet_type]',
+                             pet_breed ='$_POST[pet_breed]',
+                             age ='$_POST[age]',
+                             pet_detail ='$_POST[pet_detail]',
+                             pet_image ='$_POST[pet_image]' WHERE pet_id ='$_POST[pet_id]'";
 
 $result= $conn->query($sql_update);
 
@@ -28,6 +27,6 @@ Swal.fire({
 </script></body>';
 header("refresh: 1; url=https://petvilla.online/reservation.php");
 }
-}
+
 ?>
 </body>
