57e2e3ec0ce6850215cbfabd8a00a6aa2d6258ed
IZoom7 <crazygame1908@gmail.com>
2023-10-30 20:10:22 +0700

    93rd commit

--- edit_pet_success.php
+++ edit_pet_success.php
@@ -2,40 +2,32 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <?php
+
 require 'dbconn.php';
 
-if(isset($_POST['pet_id'])) {
-    // Validate and sanitize user input
-    $pet_id = $_POST['pet_id'];
-    $pet_name = $_POST['pet_name'];
-    $pet_type = $_POST['pet_type'];
-    $pet_breed = $_POST['pet_breed'];
-    $age = $_POST['age'];
-    $pet_detail = $_POST['pet_detail'];
-    $pet_image = $_POST['pet_image'];
+if (isset($_GET['pet_id'])) {
+$sql_update="UPDATE pets SET pet_name='$_POST[pet_name]',
+                             pet_type='$_POST[pet_type]',
+                             pet_breed='$_POST[pet_breed]',
+                             age='$_POST[age]',
+                             pet_detail='$_POST[pet_detail]',
+                             pet_image='$_POST[pet_image]' WHERE pet_id='$_POST[pet_id]'";
 
-    // Use prepared statements to prevent SQL injection
-    $sql_update = "UPDATE pets SET pet_name=?, pet_type=?, pet_breed=?, age=?, pet_detail=?, pet_image=? WHERE pet_id=?";
-    $stmt = $conn->prepare($sql_update);
-    $stmt->bind_param("ssssssi", $pet_name, $pet_type, $pet_breed, $age, $pet_detail, $pet_image, $pet_id);
+$result= $conn->query($sql_update);
 
-    if ($stmt->execute()) {
-        echo '<body><script>
-        Swal.fire({
-            icon: "success",
-            title: "แก้ไขข้อมูลสำเร็จ",
-            text: "รอซักครู่นะเหมียว..."
-        });
-        </script></body>';
-        header("refresh: 1; url=https://petvilla.online/reservation.php");
-    } else {
-        die("Error: " . $stmt->error);
-    }
+if(!$result) {
+    die("Error...: ". $conn->error);
+} else {
 
-    $stmt->close();
+echo '<body><script>
+Swal.fire({
+    icon: "success",
+    title: "แก้ไขข้อมูลสำเร็จ",
+    text: "รอซักครู่นะเหมียว..."
+});
+</script></body>';
+header("refresh: 1; url=https://petvilla.online/reservation.php");
 }
-
-$conn->close();
+}
 ?>
-
 </body>
